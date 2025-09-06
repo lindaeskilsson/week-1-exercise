3b1b3f3c231c8696db8689e1ea8601e9b07cf296
Linda Eskilsson <eskilsson.linda@hotmail.se>
2025-09-06 19:01:47 +0200

    Project exercise week 1

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("this is my project where I am about to enter all my code from the exercises from week 1");
+
+    }
+}
