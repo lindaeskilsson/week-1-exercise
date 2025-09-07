2a114acef03e0afc6c970995e6621c0ad323c741
Linda Eskilsson <eskilsson.linda@hotmail.se>
2025-09-07 11:35:59 +0200

    Project exercise week 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     public static void main(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("this is my project where I am about to enter all my code from the exercises from week 1");
-
+        System.out.println("this is my project where I am about to enter all my code from the exercises from week 1");
+        System.out.println(12);
     }
 }
