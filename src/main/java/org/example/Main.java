5b20e852545213ceedeaff7b3acdff6ecc8243cd
Linda Eskilsson <eskilsson.linda@hotmail.se>
2025-09-07 16:48:45 +0200

    Project exercise week 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,61 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("this is my project where I am about to enter all my code from the exercises from week 1");
-        System.out.println(12);
+
+
+        // 1. Grundläggande syntax, datatyper och felsökning
+
+        //1.1
+//        int x;
+//        x = 15;
+//        System.out.println(x);
+//        // 15
+//
+//        int x = 10;
+//        int y = x - 1;
+//        System.out.println(y);
+//        // 9
+
+//        int x, y;
+//        x = (int)120.954;
+//        y = x * 2;
+//        System.out.println(x + y);
+        //360
+
+//        String s = "amm";
+//        s = "fäkt";
+//        System.out.println("An" + s + "a");
+//        // Anfäkta
+
+//        String a = "an", b = "be", d= "do";
+//        System.out.println(a + b + a + d + b);
+//        anbeandobe
+
+//        int x = 2, y = 4, z = 8;
+//        System.out.println( (x * y + z) / y - z + y / (x * z / x) );
+//        //-4
+
+
+//        // 2.
+//
+//        int age = 29;
+//        double weight =70.1;
+//        char initial = 'L';
+//
+//        System.out.println("age: " + age + " Weight: " + weight +  " initial: " + initial);
+
+        //3 Skriv ett program som frågar efter användarens namn
+        // och skriver ut en hälsning på konsolen. Tex om namnet är "Kalle" ska det skriva ut "Hej Kalle!"
+//
+//        String name = System.console().readln("Vad heter du? ");
+//        System.out.println("Hej " + name + "!");
+
+        //4 Skapa ett program som beräknar ytan av en rektangel.
+
+
+
+
+
+
     }
-}
+        }
